86d04f6504af0101e4a329819cb40e2d39063595
Bona-Zeynalabid <bonazeynalabid75@gmail.com>
2025-08-10 19:45:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var container=document.getElementById("container");
 var btn=document.getElementById("btn").addEventListener('click' , ()=>{
     
     var inp=document.getElementById("input").value;
-    
+    try{
     var url=`https://restcountries.com/v3.1/name/${inp}`;
      fetch(url).then(response=>{
       return response.json();
@@ -62,8 +62,10 @@ console.log(thelast);
         }
         
  });
-
+    }catch(error){
+        container.innerHTML='<h1>No Result Found</h1>';
 });
+
 
 
 
